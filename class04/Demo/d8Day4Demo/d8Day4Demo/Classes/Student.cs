a8bdc3cb2ce69a7436b08921b01d10ffdfc09b37
Amanda Iverson <ajohnson764@gmail.com>
2019-06-13 17:55:48 -0700

    add starter code and class 04 demo!

---
+++ class04/Demo/d8Day4Demo/d8Day4Demo/Classes/Student.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace d8Day4Demo.Classes
+{
+    class Student
+    {
+        public Student(string name, int age)
+        {
+            FirstName = name;
+            Age = age;
+        }
+
+        public Student()
+        {
+            FirstName = "N/A";
+            Age = 18;
+        }
+
+        public Student(int age)
+        {
+            Age = age;
+        }
+
+        // properties 
+        public string FirstName { get; set; } = "Sally";
+
+        private int _age; // backing store
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+
+                _age = value;
+            }
+        }
+
+        // behaviors
+
+        /// <summary>
+        /// A determination of how far you have to commute to school
+        /// </summary>
+        /// <param name="milesFromSchool">number of miles you live from the school</param>
+        /// <returns>expression of concern</returns>
+        public string Commute(int milesFromSchool)
+        {
+            if(milesFromSchool <= 1)
+            {
+                return "You live very close!!";
+            }
+
+            if(milesFromSchool < 5)
+            {
+                return "You are kind of close...take the bus!";
+            }
+
+            if(milesFromSchool > 5)
+            {
+                return "You live in a far away land....";
+            }
+
+            return "You live 5 miles away!";
+        }
+
+        /// <summary>
+        /// Calculates the number of miles from the given city
+        /// </summary>
+        /// <param name="city">The city of source</param>
+        /// <returns>number of miles</returns>
+        public int Commute(string city, int desiredMiles)
+        {
+            return 0;
+        }
+
+        public string Commute(int desiredMiles, string source)
+        {
+            return "";
+        }
+
+        static public void StaticMethod()
+        {
+            Console.WriteLine("I'm Static!!");
+        }
+    }
+}
---
+++ class04/Demo/d8Day4Demo/d8Day4Demo/Program.cs
@@ -0,0 +1,67 @@
+﻿using d8Day4Demo.Classes;
+using System;
+
+namespace d8Day4Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            StudentExample();
+        }
+
+        public void MyMethod()
+        {
+            Console.WriteLine("HELLO!");
+        }
+
+        static void StudentExample()
+        {
+
+
+            int number = 10;
+            Student student = new Student("Josie", 8);
+
+            // student.FirstName = "Josie"; // "set" functionality
+            Console.WriteLine(student.FirstName); // act of "getting"
+
+            string response = student.Commute(3);
+            Console.WriteLine(response);
+
+            Student student2 = new Student();
+
+            student2.FirstName = "Belle";
+            student2.Age = 9;
+
+
+            Console.WriteLine(student2.FirstName);
+            Console.WriteLine(student2.Age);
+            Console.WriteLine(student2.Commute(16));
+
+
+            // Creating a new reference to point to an existing object
+            Student amanda = student;
+
+            Console.WriteLine("=============");
+
+            Console.WriteLine($"student firstname:{student.FirstName}");
+            Console.WriteLine($"amanda firstname:{amanda.FirstName}");
+
+
+            amanda.FirstName = "Amanda";
+            Console.WriteLine("CHANGED amanda's name to amanda");
+
+            Console.WriteLine($"student firstname:{student.FirstName}");
+            Console.WriteLine($"amanda firstname:{amanda.FirstName}");
+
+
+            // moving our reference to another object
+            amanda = student2;
+
+
+
+
+        }
+    }
+}
