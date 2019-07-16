9add52b667b664f0aea217646f2d8873357d406a
Amanda Iverson <ajohnson764@gmail.com>
2019-07-15 18:22:53 -0700

    add intro to identity demo code

---
+++ class26/demo/Cohort8CMSProject/Cohort8CMSProject/Data/BlogDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Cohort8CMSProject.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cohort8CMSProject.Data
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext(DbContextOptions<BlogDbContext>options):base(options)
+        {
+
+        }
+
+        public DbSet<Post> Posts { get; set; }
+    }
+}
