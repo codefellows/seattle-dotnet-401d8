1ecd08d0a21ac9fbdbfb634a75b3ab3b5f232d79
Amanda Iverson <ajohnson764@gmail.com>
2019-07-31 21:16:31 -0700

    add demo code and graphs

---
+++ class38/Demo/CMSDemo/CMSDemo/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CMSDemo.Models.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CMSDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IPayment _payment;
+
+        public HomeController(IPayment payment)
+        {
+            _payment = payment;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(bool yes = true)
+        {
+           string answer = _payment.Run();
+
+            return View();
+        }
+    }
+}
---
+++ class38/Demo/CMSDemo/CMSDemo/Models/Interfaces/IPayment.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMSDemo.Models.Interfaces
+{
+    public interface IPayment
+    {
+        string Run();
+    }
+}
